b86b54b40033edf70a0bb862a7b3280600a74fe4
maquinainquieta <asamimw@gmail.com>
2019-04-24 12:57:23 +0200

    Fixed dropdown boxes

--- src/topbuttons.js
+++ src/topbuttons.js
@@ -6,38 +6,42 @@ export default class TopButtons extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {};
+        this.state = {
+            toggle: true,
+            showDropdown1: false
+        };
     }
+
 
     render = () => {
         return <div>
             <ButtonGroup>
 
-            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={() => this.setState({ showDropdown1: !this.state.showDropdown1 })}>
                 <DropdownToggle caret>
                 Language
                 </DropdownToggle>
-                <DropdownMenu>
+                <DropdownMenu className={this.state.showDropdown1 ? "show" : ""}>
                 <DropdownItem>Dropdown Link</DropdownItem>
                 <DropdownItem>Dropdown Link</DropdownItem>
                 </DropdownMenu>
             </ButtonDropdown>
 
-            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={() => this.setState({ showDropdown2: !this.state.showDropdown2 })}>
             <DropdownToggle caret>
             Voice
             </DropdownToggle>
-            <DropdownMenu>
+            <DropdownMenu className={this.state.showDropdown2 ? "show" : ""}>
             <DropdownItem>Dropdown Link</DropdownItem>
             <DropdownItem>Dropdown Link</DropdownItem>
             </DropdownMenu>
             </ButtonDropdown>
 
-            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={() => this.setState({ showDropdown3: !this.state.showDropdown3 })}>
                 <DropdownToggle caret>
                 Speed
                 </DropdownToggle>
-                <DropdownMenu>
+                <DropdownMenu className={this.state.showDropdown3 ? "show" : ""}>
                 <DropdownItem>Dropdown Link</DropdownItem>
                 <DropdownItem>Dropdown Link</DropdownItem>
                 </DropdownMenu>
