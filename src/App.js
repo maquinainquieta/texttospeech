7967e7e2fdfb529ccfc99f04162f7a6da6a37490
mc-santiago <mari.c.santiago@europe.com>
2019-05-08 11:55:52 +0200

    logo added at the top and playing with background color

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
+import LogoWriteOutLoud from './LogoWriteOutLoud.png';
 import './App.css';
 import TopButton from './topbuttons.js';
+import TextArea from './TextArea'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <img src={LogoWriteOutLoud} alt="Logo Write Out Loud Text to Speech" />
       <TopButton/>
+      <TextArea/>
       </div>
     );
   }
