f7a612b54218d1436c1ece939801be8ad3eb9a3d
mc-santiago <mari.c.santiago@europe.com>
2019-05-09 15:36:45 +0200

    top buttons change from asami

--- src/topbuttons.js
+++ src/topbuttons.js
@@ -1,53 +1,67 @@
-import React, { Component } from 'react';
-import { ButtonGroup, ButtonDropdown, DropdownToggle, DropdownItem, DropdownMenu } from 'reactstrap';
+import React from "react";
+import { Dropdown } from "semantic-ui-react";
+const languageOptions = [
+  {
+    key: "English",
+    text: "English",
+    value: "English",
+  },
+  {
+    key: "Spanish",
+    text: "Spanish",
+    value: "Spanish",
+  },
+  {
+    key: "French",
+    text: "French",
+    value: "French",
+  },
+  {
+    key: "Russian",
+    text: "Russian",
+    value: "Russian",
+  }
+];
+const voiceOptions = [
+  {
+    key: "Jessica",
+    text: "Jessica",
+    value: "Jessica",
+  },
+  {
+    key: "Spanish",
+    text: "Spanish",
+    value: "Spanish",
+  },
+  {
+    key: "French",
+    text: "French",
+    value: "French",
+  },
+  {
+    key: "Russian",
+    text: "Russian",
+    value: "Russian",
+  }
+];
+const DropdownExampleOpenOnFocus = () => (
+  <React.Fragment>
+    <div class="dropdown-container">
+      <Dropdown
+        placeholder="Languages"
+        openOnFocus
+        selection
+        options={languageOptions}
+      />{" "}
+      <Dropdown
+        placeholder="Voice"
+        openOnFocus={false}
+        selection
+        options={voiceOptions}
+      />
+    </div>
+  </React.Fragment>
+);
+export default DropdownExampleOpenOnFocus;
 
 
-export default class TopButtons extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            toggle: true,
-            showDropdown1: false
-        };
-    }
-
-
-    render = () => {
-        return <div>
-            <ButtonGroup>
-
-            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={() => this.setState({ showDropdown1: !this.state.showDropdown1 })}>
-                <DropdownToggle caret>
-                Language
-                </DropdownToggle>
-                <DropdownMenu className={this.state.showDropdown1 ? "show" : ""}>
-                <DropdownItem>English</DropdownItem>
-                <DropdownItem>Spanish</DropdownItem>
-                </DropdownMenu>
-            </ButtonDropdown>
-
-            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={() => this.setState({ showDropdown2: !this.state.showDropdown2 })}>
-            <DropdownToggle caret>
-            Voice
-            </DropdownToggle>
-            <DropdownMenu className={this.state.showDropdown2 ? "show" : ""}>
-            <DropdownItem>Jessica</DropdownItem>
-            <DropdownItem>Javier</DropdownItem>
-            </DropdownMenu>
-            </ButtonDropdown>
-
-            <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={() => this.setState({ showDropdown3: !this.state.showDropdown3 })}>
-                <DropdownToggle caret>
-                Speed
-                </DropdownToggle>
-                <DropdownMenu className={this.state.showDropdown3 ? "show" : ""}>
-                <DropdownItem>Slow</DropdownItem>
-                <DropdownItem>Fast</DropdownItem>
-                </DropdownMenu>
-            </ButtonDropdown>
-
-            </ButtonGroup>
-        </div>
-    }
-}
